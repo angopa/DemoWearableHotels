d1651989517f140c6bc339dad14d98dd76ef54dc
Andres Gonzalez Paez <andres.gonzalez.paez1@gmail.com>
2016-10-16 21:49:34 -0400

    Exercise complete! LandonHotel with Maps

--- app/src/main/java/demos/android/com/craneo/landonhotels/DetailActivity.java
+++ app/src/main/java/demos/android/com/craneo/landonhotels/DetailActivity.java
@@ -5,6 +5,8 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -16,13 +18,38 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class DetailActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+
+public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private static final int ERROR_DIALOG_REQUEST = 901;
+    private static final int ZOOM_VALUE = 15;
     private Hotel hotel;
+
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        //setContentView(R.layout.activity_detail);
+
+        if(servicesOk()){
+            setContentView(R.layout.activity_detail);
+            initMap();
+            Toast.makeText(this, "Ready to map!", Toast.LENGTH_SHORT).show();
+        }else{
+            setContentView(R.layout.activity_detail);
+            Toast.makeText(this, "Map no connected", Toast.LENGTH_SHORT).show();
+        }
 
         String city = getIntent().getStringExtra("city");
 
@@ -43,6 +70,26 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
+    private void initMap() {
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    public boolean servicesOk(){
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int isAvailable = googleAPI.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS){
+            return true;
+        }else if (googleAPI.isUserResolvableError(isAvailable)){
+            googleAPI.getErrorDialog(this, isAvailable,  ERROR_DIALOG_REQUEST).show();
+        }else{
+            Toast.makeText(this, "Can't connect to mapping service", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
+
     private void displayHotelDetails(){
         TextView cityText = (TextView)findViewById(R.id.cityText);
         cityText.setText(hotel.getCity());
@@ -52,12 +99,6 @@ public class DetailActivity extends AppCompatActivity {
 
         TextView descriptionText = (TextView)findViewById(R.id.description);
         descriptionText.setText(hotel.getDescription()+"\n");
-
-        int imageResource = getResources().getIdentifier(
-                hotel.getImage(), "drawable", getPackageName());
-
-        ImageView iv = (ImageView)findViewById(R.id.imageView);
-        iv.setImageResource(imageResource);
     }
 
     public void sendNotification(View view){
@@ -108,5 +149,35 @@ public class DetailActivity extends AppCompatActivity {
         nmc.notify(notificationId, builder.build());
 
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        mMap = googleMap;
+        try {
+            geoLocale();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void geoLocale() throws IOException{
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> list = geocoder.getFromLocationName(hotel.getAddress().toString(),1);
+
+        if (list.size()>0){
+            Address add = list.get(0);
+            double lat = add.getLatitude();
+            double lng = add.getLongitude();
+
+            gotoLocation(lat, lng, ZOOM_VALUE);
+        }
+
+    }
+
+    private void gotoLocation(double lat, double lng, int zoomValue) {
+        LatLng latLng = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, zoomValue);
+        mMap.moveCamera(update);
     }
 }
